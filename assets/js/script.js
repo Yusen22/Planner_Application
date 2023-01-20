38a44351c6988eca733c3d16d2869ad2c08d17f0
Yusen22 <elijahwilsher@gmail.com>
2023-01-20 19:55:45 +0000

    fixed currentTime(). Used for loop to assign different datanumbers to timeblocks divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,30 @@
 
 var currentDay = moment().format("LL")
 
+// Sets currentDay div to display current day 
+
 $('#currentDay').text(currentDay)
+
+
+
+$(function () {
+
+    function currentTime() {
+        return moment().format("HH")
+    }
+    
+    currentTime();
+
+    var containerChildren = $('.container-fluid').children()
+
+    for (var i = 0; i < 10; i++) {
+        
+        containerChildren.eq(i).attr('data-number', containerChildren[i].i = i + 9); 
+
+        
+    }
+
+
+    // if (currentTime < )
+
+})
